256cc7fe183bbdf75efb8081f814d8533383c2e0
Wojciech Kocemba <wojciechkocemba21@gmail.com>
2026-06-13 14:35:19 +0200

    add main tf

---
+++ terraform/main.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_key_pair" "this" {
+  key_name   = "${var.project_name}-key"
+  public_key = file(var.ssh_public_key_path)
+}
